fcf504cb491623624eab36b00ef57b083fc3a426
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2018-05-31 18:20:29 +0200

    Update back4.sh

--- back4.sh
+++ back4.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+rm -rf /tmp/back4
+mkdir /tmp/back4
+echo "spleeting .."
+convert -coalesce $1 /tmp/back4/a.png
+echo ok
+dir=/tmp/back4
+
 while :                 #début de la boucle sans fin
   do
 	
-	for i in ` ls $1 | sort -n `
+	for i in ` ls $dir -v `
 		do
  			
-			#feh --bg-scale $1/$i
-			feh --bg-fill $1/$i
-			sleep 0.015
+			#feh --bg-scale $dir/$i
+			#echo "feh --bg-fill $dir/$i" >> instj.sh
+			feh --bg-fill $dir/$i 
+                        #nitrogen --set-zoom $dir/$i
+			sleep 0.010
+			#echo 'sleep 0.015' >> instj.sh
 			#clear
 		done
 
 
-done   
+done  
