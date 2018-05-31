9fab5b388642d3004f83c7637872f25fa99b1967
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2018-05-31 19:03:50 +0200

    add time var

--- back4.sh
+++ back4.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 dir=/tmp/back4
+#1=0.010
 
-if ! [[ $1 == "" ]] || ! [[ -d $dir ]] ; then 
+if ! [[ $2 == "" ]] || ! [[ -d $dir ]] ; then 
 	rm -rf $dir
 	mkdir  $dir
 	echo "spliting .."
-	convert -coalesce $1 /tmp/back4/a.png
+	convert -coalesce $2 /tmp/back4/a.png
 	echo ok
 fi
 
@@ -19,7 +20,7 @@ while :                 #début de la boucle sans fin
 			#echo "feh --bg-fill $dir/$i" >> instj.sh
 			feh --bg-fill $dir/$i 
                         #nitrogen --set-zoom $dir/$i
-			sleep 0.010
+			sleep $1
 			#echo 'sleep 0.015' >> instj.sh
 			#clear
 		done
