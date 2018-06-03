0c726e69941aa91e55f3a4fb1b23921e1285f064
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2018-06-03 16:41:04 +0200

    Update back4.sh

--- back4.sh
+++ back4.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+[ "$#" -lt "1" ] || [ "$#" -gt "2" ] && {  echo -e "ERROR : args number invalid \n $0 speed /path/name.gif" ; echo "try 0.010 as speed" ; exit 1 ; }
 dir=/tmp/back4  
 #example of speed  : 0.010
 speed=$1
