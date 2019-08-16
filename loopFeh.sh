82f9c7e0fc1e636b424f2d245b725f1f5f4cd3ff
Thomas10_10 <Thomas10_10>
2019-08-16 18:07:26 +0200

    up

--- loopFeh.sh
+++ loopFeh.sh
@@ -5,9 +5,9 @@ dir=/tmp/back4
 speed=$1
 name=$2
 
-[[ `which feh ; $?` == 0 ]] && { prog='feh --bg-fill ' }
-[[ `which xfconf-query ; $?` == 0 ]] && { prog='xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s' }
-#[[ `which osascript ; $?` == 0 ]]  && { prog='osascript -e \'tell application "Finder" to set desktop picture to POSIX file' }
+[[ `which feh ; $?` == 0 ]] && { prog='feh --bg-fill $dir/$hash/$i ' ; }
+[[ `which xfconf-query ; $?` == 0 ]] && { prog='xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s $dir/$hash/$i '; }
+[[ `which osascript ; $?` == 0 ]]  && { prog='osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$dir/$hash/$i\" " '; }
 #osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/davidwalsh/Downloads/pubg-background.jpg"'
 
 [[ "$name" == "" ]] && { name=$speed ; speed=${name##*-} ; }
@@ -18,5 +18,5 @@ hash=`md5sum $name | cut -f1 -d" "`
 
 [[ ! -d $dir/$hash ]] && { mkdir $dir/$hash ; echo "spliting .." ; convert -coalesce $name $dir/$hash/$hash.png ; echo ok ; }
 
-while : ; do for i in ` ls $dir/$hash -v ` ; do $prog $dir/$hash/$i ; sleep $speed ; done ; done    
+while : ; do for i in ` ls $dir/$hash -v ` ; do eval $prog ; sleep $speed ; done ; done    
    
