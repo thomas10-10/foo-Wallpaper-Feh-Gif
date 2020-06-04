88622ecd4942d6182680e2c7cb869656293811c9
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2020-06-04 23:34:34 +0200

    Update back4.sh

--- back4.sh
+++ back4.sh
@@ -2,6 +2,7 @@
 select1='feh --bg-fill'
 select2='xwallpaper --zoom'
 select3='xloadimage -onroot -fullscreen'
+select4='xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s'
 prog=$select1
 [ "$#" -lt "1" ] || [ "$#" -gt "2" ] && {  echo -e "ERROR : args number invalid \n $0 speed /path/name.gif" ; echo "try 0.010 as speed" ; exit 1 ; }
 dir=/tmp/back4  
