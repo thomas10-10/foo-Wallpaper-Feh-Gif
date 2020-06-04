b6e20b97ba486b3d5cfa57dff5fa37bb12da16ab
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2020-06-04 23:20:25 +0200

    Update back4.sh

--- back4.sh
+++ back4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-prog='feh --bg-fill'
-#prog='xwallpaper --zoom'
-#prog='xloadimage -onroot -fullscreen /path/to/background_image'
-
+select1='feh --bg-fill'
+select2='xwallpaper --zoom'
+select3='xloadimage -onroot -fullscreen /path/to/background_image'
+prog=$select1
 [ "$#" -lt "1" ] || [ "$#" -gt "2" ] && {  echo -e "ERROR : args number invalid \n $0 speed /path/name.gif" ; echo "try 0.010 as speed" ; exit 1 ; }
 dir=/tmp/back4  
 #example of speed  : 0.010
