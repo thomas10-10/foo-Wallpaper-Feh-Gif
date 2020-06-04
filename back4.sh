665dddf8d5fc56f00194d5d697ade20c6584196d
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2020-06-04 23:12:14 +0200

    Update back4.sh

--- back4.sh
+++ back4.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+prog='feh --bg-fill'
+#prog='xwallpaper --zoom'
+#prog='xloadimage -onroot -fullscreen /path/to/background_image'
+
 [ "$#" -lt "1" ] || [ "$#" -gt "2" ] && {  echo -e "ERROR : args number invalid \n $0 speed /path/name.gif" ; echo "try 0.010 as speed" ; exit 1 ; }
 dir=/tmp/back4  
 #example of speed  : 0.010
@@ -14,5 +18,5 @@ hash=`md5sum $name | cut -f1 -d" "`
 
 [[ ! -d $dir/$hash ]] && { mkdir $dir/$hash ; echo "spliting .." ; convert -coalesce $name $dir/$hash/$hash.png ; echo ok ; }
 
-while : ; do for i in ` ls $dir/$hash -v ` ; do feh --bg-fill $dir/$hash/$i ; sleep $speed ; done ; done    
+while : ; do for i in ` ls $dir/$hash -v ` ; do $prog $dir/$hash/$i ; sleep $speed ; done ; done    
    
