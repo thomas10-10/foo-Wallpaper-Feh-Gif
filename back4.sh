3a09e564eba081930d722f8ad5b968270b1d3886
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2020-10-26 21:33:36 +0100

    Update back4.sh
    add hsetroot method

--- back4.sh
+++ back4.sh
@@ -3,6 +3,8 @@ select1='feh --bg-fill'
 select2='xwallpaper --zoom'
 select3='xloadimage -onroot -fullscreen'
 select4='xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s'
+select5='hsetroot -full'
+
 prog=$select1
 [ "$#" -lt "1" ] || [ "$#" -gt "2" ] && {  echo -e "ERROR : args number invalid \n $0 speed /path/name.gif" ; echo "try 0.010 as speed" ; exit 1 ; }
 dir=/tmp/back4  
