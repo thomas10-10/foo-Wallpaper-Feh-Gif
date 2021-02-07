60a226f38dd2e5affe9fb1a46ec44c762cb67776
Raphael Henrique <raphahenrique777@gmail.com>
2021-02-06 22:25:17 -0300

    set 'auto' option for automatically calculating the speed (30 frames per second)

--- back4.sh
+++ back4.sh
@@ -12,7 +12,6 @@ dir=/tmp/back4
 speed=$1
 name=$2
 
-
 [[ "$name" == "" ]] && { name=$speed ; speed=${name##*-} ; }
 
 hash=`md5sum $name | cut -f1 -d" "`
@@ -20,6 +19,13 @@ hash=`md5sum $name | cut -f1 -d" "`
 [[ ! -d $dir ]] && mkdir $dir 
 
 [[ ! -d $dir/$hash ]] && { mkdir $dir/$hash ; echo "spliting .." ; convert -coalesce $name $dir/$hash/$hash.png ; echo ok ; }
+
+if [ "$1" == "auto" ]; then
+    frames=($(ls -v "$dir/$hash"))
+    amount_of_frames=${#frames[@]}
+    frames_per_second=30
+    speed=$(bc <<< "scale=3; ($amount_of_frames/$frames_per_second) * (1/$frames_per_second)")
+fi
 
 while : ; do for i in ` ls $dir/$hash -v ` ; do $prog $dir/$hash/$i ; sleep $speed ; done ; done    
    
