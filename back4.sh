d3ff16da1f29143001c0663883e47d46f193066c
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2022-02-13 15:45:44 +0100

    Update back4.sh

--- back4.sh
+++ back4.sh
@@ -1,10 +1,10 @@
 #!/bin/env bash
-select1='feh --bg-fill --no-fehbg'
-select2='xwallpaper --zoom'
-select3='xloadimage -onroot -fullscreen'
-select4='xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s'
-select5='hsetroot -full'
-select6='gsettings set org.gnome.desktop.background picture-uri'
+select1='feh --bg-fill --no-fehbg '
+select2='xwallpaper --zoom '
+select3='xloadimage -onroot -fullscreen '
+select4='xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s '
+select5='hsetroot -full '
+select6='gsettings set org.gnome.desktop.background picture-uri file:///'
 
 prog=$select1
 [ "$#" -lt "1" ] || [ "$#" -gt "2" ] && {  echo -e "ERROR : args number invalid \n $0 speed /path/name.gif" ; echo "try 0.010 as speed" ; exit 1 ; }
@@ -28,5 +28,5 @@ if [ "$1" == "auto" ]; then
     speed=$(bc <<< "scale=3; ($amount_of_frames/$frames_per_second) * (1/$frames_per_second)")
 fi
 
-while : ; do for i in ` ls $dir/$hash -v ` ; do $prog $dir/$hash/$i ; sleep $speed ; done ; done    
+while : ; do for i in ` ls $dir/$hash -v ` ; do $prog$dir/$hash/$i ; sleep $speed ; done ; done    
    
