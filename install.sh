c2c6f06500d0d9ae0f8d009ca93739007d306f5f
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2022-02-18 14:41:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
+sudo mkdir -p /usr/local/share/gif-wallpaper
+sudo curl -Lo /usr/local/share/gif-wallpaper/loading.png  https://github.com/thomas10-10/foo-Wallpaper-Feh-Gif/raw/master/gif/loading.png
 sudo curl -Lo /usr/local/bin/back4.sh  https://raw.githubusercontent.com/thomas10-10/foo-Wallpaper-Feh-Gif/master/back4.sh && sudo chmod +x /usr/local/bin/back4.sh
